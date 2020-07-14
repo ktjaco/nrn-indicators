b4e117a287f2787feee44f7a4d6cefa8b96a7ef9
ktjaco <kent.jacobs@gmail.com>
2020-07-14 11:21:55 -0400

    implement overlap percent calculation

--- src/compare_overlap.py
+++ src/compare_overlap.py
@@ -1,12 +1,7 @@
-import fiona
 import geopandas as gpd
-import os, sys
+import sys
 import logging
 from datetime import datetime
-
-sys.path.insert(1, os.path.join(sys.path[0], ".."))
-# import helpers
-# import qgis_processing
 
 # Set logger.
 logger = logging.getLogger()
@@ -18,58 +13,53 @@ logger.addHandler(handler)
 
 startTime = datetime.now()
 
-def main(network, nrn, nrn_layer):
+def main(network, net_layer, nrn, nrn_layer):
 
     # command line system arguments
     network = (sys.argv[1])
-    nrn = (sys.argv[2])
-    nrn_layer = (sys.argv[3])
-    # output = (sys.argv[4])
+    net_layer = (sys.argv[2])
+    nrn = (sys.argv[3])
+    nrn_layer = (sys.argv[4])
 
     # load the two datasets that will be compared
-    net_gdf = gpd.read_file(network)
+    logger.info("Reading tested and NRN datasets.")
+    net_gdf = gpd.read_file(network, layer=net_layer)
     nrn = gpd.read_file(nrn, layer=nrn_layer)
 
     # convert to projects CRS
+    logger.info("Converting tested and NRN datasets to EPSG:3348.")
     nrn = nrn.to_crs("EPSG:3348")
     net_gdf = net_gdf.to_crs("EPSG:3348")
 
     # buffer "true" road network
-    nrn_buffer = nrn.buffer(10)
+    logger.info("Performing a 10 metre buffer on the NRN dataset.")
+    buff_width = 10
+    nrn_buffer = nrn.buffer(buff_width)
 
     logger.info("Creating buffer gdf.")
     nrn_buffer_gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries(nrn_buffer))
-    # nrn_buffer_gdf = nrn_buffer_gdf.to_crs("EPSG:3348")
 
-    # clip tested road network
-    # logger.info("Simplify buffer.")
-    # nrn_buffer_gdf_sim = nrn_buffer_gdf.simplify(0.2, preserve_topology=True)
-
-    # net_gdf_sindex = net_gdf.sindex
-    # nrn_buffer_gdf_sim_sindex = nrn_buffer_gdf_sim.sindex
-
-    logger.info("Clipping...")
+    logger.info("Clipping the tested dataset with NRN dataset.")
     nrn_clip = gpd.overlay(net_gdf, nrn_buffer_gdf, how="intersection")
 
+    nrn_clip_length = nrn_clip.length.sum()
+    net_gdf_length = net_gdf.length.sum()
 
-    logger.info("Printing...")
-    nrn_clip.to_file("../data/interim/output.gpkg", layer="nrn_clip", driver="GPKG")
-    net_gdf.to_file("../data/interim/output.gpkg", layer="network", driver="GPKG")
-    nrn_buffer_gdf.to_file("../data/interim/output.gpkg", layer="nrn_buffer", driver="GPKG")
+    logger.info("Calculating tested network overlap percentage.")
+    percentage = (nrn_clip_length / net_gdf_length) * 100
+    percentage = round(percentage, 2)
 
-    sys.exit(1)
-
-
-
+    logger.info("{}% of the tested road network falls within {} metres of the NRN dataset."
+                .format(percentage, buff_width))
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("ERROR: You must supply 4 arguments. "
-              "Example: python..")
+              "Example: python3 [GPKG path] [GPKG layer] [NRN path] [NRN layer]")
         sys.exit(1)
 
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
 # output execution time
 print("Total execution time: ", datetime.now() - startTime)
